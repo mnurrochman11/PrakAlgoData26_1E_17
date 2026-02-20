b3467b7089a59474cb4a7434139e4a933e64cdfc
muhammadnurrochman <kutil1103@gmail.com>
2026-02-20 14:42:11 +0700

    P2_Percobaan2

---
+++ P2/mahasiswaMain17.java
@@ -0,0 +1,15 @@
+public class mahasiswaMain17 {
+
+    public static void main(String[] args) {
+        mahasiswa17 mhsi_17 = new mahasiswa17();
+        mhsi_17.nama17 = "Muhammad Ali Farhan";
+        mhsi_17.nim17 = "2241720171";
+        mhsi_17.kelas17 = "S1 2J";
+        mhsi_17.ipk17 = 3.55;
+
+        mhsi_17.tampiklkanInformasi17();
+        mhsi_17.ubahKelas17("S1 2K");
+        mhsi_17.updateIpk17(3.60);
+        mhsi_17.tampiklkanInformasi17();
+    }
+}
