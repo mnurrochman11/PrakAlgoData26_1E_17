743ca827d836e8e97221ac28faecfcc204fd18fb
muhammadnurrochman <kutil1103@gmail.com>
2026-02-26 10:46:52 +0700

    P3_Percobaan3.4_soal4

--- P3/MataKuliahDemo.java
+++ P3/MataKuliahDemo.java
@@ -5,17 +5,21 @@ import java.util.Scanner;
 public class MataKuliahDemo {
     public static void main(String[] args) {
         Scanner nur17 = new Scanner(System.in);
-        MataKuliah17[] arrayOfMataKuliah17 = new MataKuliah17[3];
         String kode17, nama17, dummy17;
         int sks17, jumlahJam17;
 
-        for (int i = 0; i < 3; i++) {
+        System.out.print("Masukkan jumlah mata kuliah: ");
+        int jumlah = Integer.parseInt(nur17.nextLine());
+
+        MataKuliah17[] arrayOfMataKuliah17 = new MataKuliah17[jumlah];
+
+        for (int i = 0; i < jumlah; i++) {
             System.out.println("Masukkan Data Mata Kuliah ke-" + (i + 1));
             arrayOfMataKuliah17[i] = new MataKuliah17();
             arrayOfMataKuliah17[i].tambahData();
         }
 
-        for (int i = 0; i < 3; i++) {
+        for (int i = 0; i < jumlah; i++) {
             System.out.println("Data Mata Kuliah ke-" + (i + 1));
             arrayOfMataKuliah17[i].cetakInfo();
         }
