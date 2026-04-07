54fcb912896c8f46dca12fe4e7da62b4364a6b4d
muhammadnurrochman <kutil1103@gmail.com>
2026-04-07 12:24:40 +0700

    P6_53

---
+++ P6/MahasiswaBerprestasi17.java
@@ -0,0 +1,39 @@
+package P6;
+
+public class MahasiswaBerprestasi17 {
+    // Mahasiswa17[] listMhs = new Mahasiswa17[5];
+    Mahasiswa17[] listMhs;
+    int idx;
+
+    MahasiswaBerprestasi17(int n){
+        listMhs = new Mahasiswa17[n];
+    }
+    void tambah(Mahasiswa17 m) {
+        if (idx < listMhs.length) {
+            listMhs[idx] = m;
+            idx++;
+        } else {
+            System.out.println("data sudah penuh");
+        }
+    }
+
+    void tampil() {
+        for (Mahasiswa17 m : listMhs) {
+            m.tampilInformasi();
+            System.out.println("-------------------");
+        }
+    }
+
+    void bubbleSort() {
+        for (int i = 0; i < listMhs.length - 1; i++) {
+            for (int j = 1; j < listMhs.length - i; j++) {
+                if (listMhs[j].ipk > listMhs[j - 1].ipk) {
+                    Mahasiswa17 tmp = listMhs[j];
+                    listMhs[j] = listMhs[j - 1];
+                    listMhs[j - 1] = tmp;
+                }
+            }
+        }
+    }
+
+}
---
+++ P6/MahasiswaDemo17.java
@@ -0,0 +1,54 @@
+package P6;
+import java.util.Scanner;
+
+public class MahasiswaDemo17 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // MahasiswaBerprestasi17 list = new MahasiswaBerprestasi17();
+        // Mahasiswa17 m1 = new Mahasiswa17("123", "Zidan", "2A", 3.2);
+        // Mahasiswa17 m2 = new Mahasiswa17("124", "Ayu", "2A", 3.5);
+        // Mahasiswa17 m3 = new Mahasiswa17("125", "Sofi", "2A", 3.1);
+        // Mahasiswa17 m4 = new Mahasiswa17("126", "Sita", "2A", 3.9);
+        // Mahasiswa17 m5 = new Mahasiswa17("127", "Mika", "2A", 3.7);
+
+        // list.tambah(m1);
+        // list.tambah(m2);
+        // list.tambah(m3);
+        // list.tambah(m4);
+        // list.tambah(m5);
+        
+        System.out.print("Masukkan jumlah mahasiswa: ");
+        int n = sc.nextInt();
+        sc.nextLine();
+        
+        MahasiswaBerprestasi17 list = new MahasiswaBerprestasi17(n);
+
+        for (int i = 0; i < n; i++) {
+            System.out.println("Data mahasiswa ke-" + (i+1));
+
+            System.out.print("NIM   : ");
+            String nim = sc.nextLine();
+
+            System.out.print("Nama  : ");
+            String nama = sc.nextLine();
+
+            System.out.print("Kelas : ");
+            String kelas = sc.nextLine();
+
+            System.out.print("IPK   : ");
+            double ipk = sc.nextDouble();
+            sc.nextLine();
+
+            Mahasiswa17 m = new Mahasiswa17(nim, nama, kelas, ipk);
+            list.tambah(m);
+        }
+
+        System.out.println("Data mahasiswa sebelum sorting : ");
+        list.tampil();
+        list.bubbleSort();
+
+        System.out.println("Data mahasiswa setelah sorting berdasarkan IPK (DESC) : ");
+        list.tampil();
+    }
+}
