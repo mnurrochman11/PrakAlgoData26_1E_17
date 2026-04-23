77a06bb948289355c333b6605fa35da2bb025b28
muhammadnurrochman <kutil1103@gmail.com>
2026-04-23 09:04:17 +0700

    P9_Percobaan1

---
+++ P9/Mahasiswa17.java
@@ -0,0 +1,17 @@
+package P9;
+
+public class Mahasiswa17 {
+    String nama, nim, kelas;
+    int nilai;
+
+    Mahasiswa17(String nama, String nim, String kelas) {
+        this.nama = nama;
+        this.nim = nim;
+        this.kelas = kelas;
+        nilai = -1;
+    }
+
+    void tugasDinilai(int nilai) {
+        this.nilai = nilai;
+    }
+}
