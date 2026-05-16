ba67f76bfa43a3b206244d76c96010ff4e0e4df7
muhammadnurrochman <kutil1103@gmail.com>
2026-05-16 23:36:36 +0700

    P11_2.2

--- P11/SLLMain17.java
+++ P11/SLLMain17.java
@@ -8,50 +8,62 @@ public class SLLMain17 {
         Scanner sc = new Scanner(System.in);
         SingleLinkedList17 sll = new SingleLinkedList17();
 
-        // Mahasiswa17 mhs1 = new Mahasiswa17("21212203", "Dirga", "4D", 3.6);
-        // Mahasiswa17 mhs2 = new Mahasiswa17("23212201", "Bimon", "2B", 3.8);
-        // Mahasiswa17 mhs3 = new Mahasiswa17("22212202", "Cintia", "3C", 3.5);
-        // Mahasiswa17 mhs4 = new Mahasiswa17("24212200", "Alvaro", "1A", 4.0);
+        Mahasiswa17 mhs1 = new Mahasiswa17("21212203", "Dirga", "4D", 3.6);
+        Mahasiswa17 mhs2 = new Mahasiswa17("23212201", "Bimon", "2B", 3.8);
+        Mahasiswa17 mhs3 = new Mahasiswa17("22212202", "Cintia", "3C", 3.5);
+        Mahasiswa17 mhs4 = new Mahasiswa17("24212200", "Alvaro", "1A", 4.0);
 
-        // sll.print();
+        sll.print();
 
-        // sll.addFirst(mhs1);
-        // sll.print();
+        sll.addFirst(mhs1);
+        sll.print();
 
-        // sll.addLast(mhs4);
-        // sll.print();
+        sll.addLast(mhs4);
+        sll.print();
 
-        // sll.insertAfter("Dirga", mhs3);
+        sll.insertAfter("Dirga", mhs3);
 
-        // sll.insertAt(2, mhs2);
+        sll.insertAt(2, mhs2);
 
-        // sll.print();
-        for (int i = 1; i <= 4; i++) {
+        sll.print();
+        // for (int i = 1; i <= 4; i++) {
 
-            System.out.println("Data Mahasiswa ke-" + i);
+        //     System.out.println("Data Mahasiswa ke-" + i);
 
-            System.out.print("NIM   : ");
-            String nim = sc.nextLine();
+        //     System.out.print("NIM   : ");
+        //     String nim = sc.nextLine();
 
-            System.out.print("Nama  : ");
-            String nama = sc.nextLine();
+        //     System.out.print("Nama  : ");
+        //     String nama = sc.nextLine();
 
-            System.out.print("Kelas : ");
-            String kelas = sc.nextLine();
+        //     System.out.print("Kelas : ");
+        //     String kelas = sc.nextLine();
 
-            System.out.print("IPK   : ");
-            double ipk = sc.nextDouble();
-            sc.nextLine();
+        //     System.out.print("IPK   : ");
+        //     double ipk = sc.nextDouble();
+        //     sc.nextLine();
 
-            Mahasiswa17 mhs = new Mahasiswa17(nim, nama, kelas, ipk);
+        //     Mahasiswa17 mhs = new Mahasiswa17(nim, nama, kelas, ipk);
 
-            if (i == 1) {
-                sll.addFirst(mhs);
-            } else {
-                sll.addLast(mhs);
-            }
+        //     if (i == 1) {
+        //         sll.addFirst(mhs);
+        //     } else {
+        //         sll.addLast(mhs);
+        //     }
 
-            sll.print();
-        }
+        //     sll.print();
+        // }
+
+        System.out.println("data index 1 : ");
+        sll.getData17(1);
+
+        System.out.println("data mahasiswa an Bimon berada pada index : " + sll.indexOf17("bimon"));
+        System.out.println();
+
+        sll.removeFirst17();
+        sll.removeLast17();
+        sll.print();
+        sll.removeAt17(0);
+        sll.print();
     }
 }
--- P11/SingleLinkedList17.java
+++ P11/SingleLinkedList17.java
@@ -80,4 +80,96 @@ public class SingleLinkedList17 {
         }
     }
 
+    public void getData17(int index) {
+        NodeMahasiswa17 tmp = head;
+        for (int i = 0; i < index; i++) {
+            tmp = tmp.next;
+        }
+        tmp.data.tampilInformasi();
+        ;
+    }
+
+    public int indexOf17(String key) {
+        NodeMahasiswa17 tmp = head;
+        int index = 0;
+        while (tmp != null && !tmp.data.nama.equalsIgnoreCase(key)) {
+            tmp = tmp.next;
+            index++;
+        }
+        if (tmp == null) {
+            return -1;
+        } else {
+            return index;
+        }
+    }
+
+    public void removeFirst17() {
+        if (isEmpty()) {
+            System.out.println("Linked List masih kosong, tidak dapat dihapus!");
+        } else if (head == tail) {
+            head = tail = null;
+        } else {
+            head = head.next;
+        }
+    }
+
+    public void removeLast17() {
+        if (isEmpty()) {
+            System.out.println("Linked List masih kosong, tidak dapat dihapus!");
+        } else if (head == tail) {
+            head = tail = null;
+        } else {
+            NodeMahasiswa17 temp = head;
+            while (temp.next != tail) {
+                temp = temp.next;
+            }
+            temp.next = null;
+            tail = temp;
+        }
+    }
+
+    public void remove17(String key) {
+        if (isEmpty()) {
+            System.out.println("Linked List masih kosong, tidak dapat dihapus!");
+        } else {
+            NodeMahasiswa17 temp = head;
+
+            while (temp != null) {
+
+                if ((temp.data.nama.equalsIgnoreCase(key)) && (temp == head)) {
+                    this.removeFirst17();
+                    break;
+
+                } else if (temp.next != null &&
+                        temp.next.data.nama.equalsIgnoreCase(key)) {
+
+                    temp.next = temp.next.next;
+
+                    if (temp.next == null) {
+                        tail = temp;
+                    }
+
+                    break;
+                }
+
+                temp = temp.next;
+            }
+        }
+    }
+
+    public void removeAt17(int index) {
+        if (index == 0) {
+            removeFirst17();
+        } else {
+            NodeMahasiswa17 temp = head;
+            for (int i = 0; i < index - 1; i++) {
+                temp = temp.next;
+            }
+            temp.next = temp.next.next;
+            if (temp.next == null) {
+                tail = temp;
+            }
+        }
+    }
+
 }
