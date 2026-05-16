2f3f3ea89b173c29458d3458d8209d1cd5436db8
muhammadnurrochman <kutil1103@gmail.com>
2026-05-16 22:52:34 +0700

    P11_2.1.2

--- P11/SLLMain17.java
+++ P11/SLLMain17.java
@@ -1,27 +1,57 @@
 package P11;
 
+import java.util.Scanner;
+
 public class SLLMain17 {
     public static void main(String[] args) {
 
+        Scanner sc = new Scanner(System.in);
         SingleLinkedList17 sll = new SingleLinkedList17();
 
-        Mahasiswa17 mhs1 = new Mahasiswa17("21212203", "Dirga", "4D", 3.6);
-        Mahasiswa17 mhs2 = new Mahasiswa17("23212201", "Bimon", "2B", 3.8);
-        Mahasiswa17 mhs3 = new Mahasiswa17("22212202", "Cintia", "3C", 3.5);
-        Mahasiswa17 mhs4 = new Mahasiswa17("24212200", "Alvaro", "1A", 4.0);
+        // Mahasiswa17 mhs1 = new Mahasiswa17("21212203", "Dirga", "4D", 3.6);
+        // Mahasiswa17 mhs2 = new Mahasiswa17("23212201", "Bimon", "2B", 3.8);
+        // Mahasiswa17 mhs3 = new Mahasiswa17("22212202", "Cintia", "3C", 3.5);
+        // Mahasiswa17 mhs4 = new Mahasiswa17("24212200", "Alvaro", "1A", 4.0);
 
-        sll.print();
+        // sll.print();
 
-        sll.addFirst(mhs1);
-        sll.print();
+        // sll.addFirst(mhs1);
+        // sll.print();
 
-        sll.addLast(mhs4);
-        sll.print();
+        // sll.addLast(mhs4);
+        // sll.print();
 
-        sll.insertAfter("Dirga", mhs3);
+        // sll.insertAfter("Dirga", mhs3);
 
-        sll.insertAt(2, mhs2);
+        // sll.insertAt(2, mhs2);
 
-        sll.print();
+        // sll.print();
+        for (int i = 1; i <= 4; i++) {
+
+            System.out.println("Data Mahasiswa ke-" + i);
+
+            System.out.print("NIM   : ");
+            String nim = sc.nextLine();
+
+            System.out.print("Nama  : ");
+            String nama = sc.nextLine();
+
+            System.out.print("Kelas : ");
+            String kelas = sc.nextLine();
+
+            System.out.print("IPK   : ");
+            double ipk = sc.nextDouble();
+            sc.nextLine();
+
+            Mahasiswa17 mhs = new Mahasiswa17(nim, nama, kelas, ipk);
+
+            if (i == 1) {
+                sll.addFirst(mhs);
+            } else {
+                sll.addLast(mhs);
+            }
+
+            sll.print();
+        }
     }
 }
